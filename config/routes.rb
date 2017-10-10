7ecb0cca7bb9301a774587e5328971420e02303c
aman_barbaria <aman@pepo.com>
2017-10-10 23:59:34 +0530

    route for api

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  scope '/user', controller: 'user' do
+  scope 'api/user', controller: 'user' do
     match 'sign-up' => :sign_up, via: :POST
     match 'login' => :login, via: :POST
   end
 
-  scope '/admin', controller: 'admin' do
+  scope 'api/admin', controller: 'admin' do
     match 'login' => :password_auth, via: :POST
     match 'mfa' => :multifactor_auth, via: :POST
   end

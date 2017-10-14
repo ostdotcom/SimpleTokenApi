9f448fc493494a3bcc512924eb942e52a6656478
sunil <sunil@hem.com>
2017-10-15 01:45:34 +0530

    Moved logs tables in logs db

--- db/migrate/20171010061659_create_admins.rb
+++ db/migrate/20171010061659_create_admins.rb
@@ -5,10 +5,10 @@ class CreateAdmins < DbMigrationConnection
     run_migration_for_db(EstablishSimpleTokenAdminDbConnection.config_key) do
 
       create_table :admins do |t|
+        t.column :admin_secret_id, :integer, limit: 8, null: false
         t.column :email, :string, null: false
         t.column :password, :string, null: false
         t.column :name, :string, null: false
-        t.column :admin_secret_id, :integer, limit: 8, null: false
         t.column :last_otp_at, :integer, null: true, limit: 8
         t.column :status, :tinyint, limit: 1, null: false, default: 1
         t.timestamps

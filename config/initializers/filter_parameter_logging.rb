867782a1a2442f91895cc11b85d10aeffa53eff9
aman_barbaria <aman@pepo.com>
2017-10-24 00:36:48 +0530

    filter parameter for logging

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+Rails.application.config.filter_parameters += [:password, :first_name, :last_name, :birthdate, :street_address, :city, :state, :country,
+                                               :postal_code, :ethereum_address, :estimated_participation_amount, :passport_number, :nationality]

f8070008c0acb155ab1c0d77a0a1d6dfab06e949
aman_barbaria <aman@pepo.com>
2017-10-26 14:31:36 +0530

    migration revert

--- db/migrate/20171025150830_add_user_kyc_detail_whitelist_column.rb
+++ db/migrate/20171025150830_add_user_kyc_detail_whitelist_column.rb
@@ -3,7 +3,6 @@ class AddUserKycDetailWhitelistColumn < DbMigrationConnection
 
     run_migration_for_db(EstablishSimpleTokenUserDbConnection.config_key) do
       add_column :user_kyc_details, :whitelist_status, :tinyint, limit: 1, null: false, after: :admin_status
-      add_column :user_kyc_details, :admin_action_type, :tinyint, limit: 1, null: false, after: :whitelist_status
     end
   end
 
@@ -12,7 +11,6 @@ class AddUserKycDetailWhitelistColumn < DbMigrationConnection
 
     run_migration_for_db(EstablishSimpleTokenUserDbConnection.config_key) do
       remove_column :user_kyc_details, :whitelist_status
-      remove_column :user_kyc_details, :admin_action_type
     end
 
   end

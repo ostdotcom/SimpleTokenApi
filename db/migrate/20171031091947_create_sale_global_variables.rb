7a0b477d1d5ec056bdff2e3e7e5a8cba96fdc490
aman_barbaria <aman@pepo.com>
2017-10-31 15:31:04 +0530

    migration

--- db/migrate/20171031091947_create_sale_global_variables.rb
+++ db/migrate/20171031091947_create_sale_global_variables.rb
@@ -3,14 +3,14 @@ class CreateSaleGlobalVariables < DbMigrationConnection
   def up
     run_migration_for_db(EstablishSimpleTokenContractInteractionsDbConnection.config_key) do
       create_table :sale_global_variables do |t|
-        t.column :variable_kind, :string, limit: 50
+        t.column :variable_kind, :tinyint, limit: 50
         t.column :variable_data, :integer
         t.timestamps
       end
 
       execute "insert into sale_global_variables (variable_kind, variable_data, created_at, updated_at) values
-            ('#{GlobalConstant::SaleGlobalVariable.sale_ended_variable_kind}', 0, '#{Time.now.to_s(:db)}', '#{Time.now.to_s(:db)}'),
-            ('#{GlobalConstant::SaleGlobalVariable.last_block_processed_variable_kind}', 0, '#{Time.now.to_s(:db)}', '#{Time.now.to_s(:db)}');"
+            ('#{SaleGlobalVariable.variable_kinds[GlobalConstant::SaleGlobalVariable.sale_ended_variable_kind]}', 0, '#{Time.now.to_s(:db)}', '#{Time.now.to_s(:db)}'),
+            ('#{SaleGlobalVariable.variable_kinds[GlobalConstant::SaleGlobalVariable.last_block_processed_variable_kind]}', 0, '#{Time.now.to_s(:db)}', '#{Time.now.to_s(:db)}');"
 
     end
 

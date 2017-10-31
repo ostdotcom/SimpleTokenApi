367dfd087defaf10a0dd49f98273556c2fd7cf7c
aman_barbaria <aman@pepo.com>
2017-10-31 19:01:00 +0530

    sale end detail route

--- db/migrate/20171031091947_create_sale_global_variables.rb
+++ db/migrate/20171031091947_create_sale_global_variables.rb
@@ -3,12 +3,12 @@ class CreateSaleGlobalVariables < DbMigrationConnection
   def up
     run_migration_for_db(EstablishSimpleTokenContractInteractionsDbConnection.config_key) do
       create_table :sale_global_variables do |t|
-        t.column :variable_kind, :tinyint, limit: 50
-        t.column :variable_data, :integer
+        t.column :variable_kind, :tinyint, null: false, limit: 4
+        t.column :variable_data, :integer, null: false
         t.timestamps
       end
 
-      execute "insert into sale_global_variables (variable_kind, variable_data, created_at, updated_at) values
+      execute "INSERT INTO sale_global_variables (variable_kind, variable_data, created_at, updated_at) VALUES
             ('#{SaleGlobalVariable.variable_kinds[GlobalConstant::SaleGlobalVariable.sale_ended_variable_kind]}', 0, '#{Time.now.to_s(:db)}', '#{Time.now.to_s(:db)}'),
             ('#{SaleGlobalVariable.variable_kinds[GlobalConstant::SaleGlobalVariable.last_block_processed_variable_kind]}', 0, '#{Time.now.to_s(:db)}', '#{Time.now.to_s(:db)}');"
 

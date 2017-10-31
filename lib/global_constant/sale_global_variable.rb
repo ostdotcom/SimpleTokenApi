0b8c9644896bffaf9028015130aab75186552407
aman_barbaria <aman@pepo.com>
2017-10-31 15:06:50 +0530

    sale ended global variable in db

---
+++ lib/global_constant/sale_global_variable.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+module GlobalConstant
+
+  class SaleGlobalVariable
+
+    class << self
+
+      def sale_ended_variable_kind
+        'sale_ended'
+      end
+
+      def last_block_processed_variable_kind
+        'last_block_processed'
+      end
+
+    end
+
+  end
+
+end

558b8611c83b8c3af403891772c05e255e92e079
Alpesh <alpesh@pepo.com>
2017-10-31 20:57:38 +0530

    syntex error fix.

--- app/services/user_management/get_token_sale_foundation_address.rb
+++ app/services/user_management/get_token_sale_foundation_address.rb
@@ -104,9 +104,9 @@ module UserManagement
     # @return [Boolean]
     #
     def is_sale_active_for_user?
-      (GlobalConstant::TokenSale.st_token_sale_active_status
-      && !GlobalConstant::TokenSale.is_sale_ended?
-      && (current_time >= sale_start_time))
+      (GlobalConstant::TokenSale.st_token_sale_active_status &&
+          !GlobalConstant::TokenSale.is_sale_ended? &&
+          (current_time >= sale_start_time))
     end
 
     # is ethereum address whitelist complete

1b7cfcb7f35ae1c6508172cf94fdb0599c6c9ab9
kedarchandrayan <kedar@pepo.com>
2017-11-10 23:17:16 +0530

    migration changes

--- db/migrate/20171025145824_create_kyc_whitelist_logs_table.rb
+++ db/migrate/20171025145824_create_kyc_whitelist_logs_table.rb
@@ -5,7 +5,7 @@ class CreateKycWhitelistLogsTable < DbMigrationConnection
       create_table :kyc_whitelist_logs do |t|
         t.column :ethereum_address, :string, limit: 255, null: false
         t.column :phase, :tinyint, limit: 1, null: false
-        t.column :transaction_hash, :string, limit: 127, null: false
+        t.column :transaction_hash, :string, limit: 255, null: false
         t.column :status, :tinyint, limit: 1, null: false
         t.column :is_attention_needed, :tinyint, limit: 1, null: false, default: 0
         t.timestamps

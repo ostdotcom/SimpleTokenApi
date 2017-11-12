a6ed8a2af9fe39e76850b669ecf54961e3e8c009
Alpesh <alpesh@pepo.com>
2017-11-12 21:54:31 +0530

    Fetch All time total on sale daily dash

---
+++ app/services/admin_management/kyc/dashboard/contract_events.rb
@@ -0,0 +1,123 @@
+module AdminManagement
+
+  module Kyc
+
+    module Dashboard
+
+      class ContractEvents < ServicesBase
+
+        # Initialize
+        #
+        # * Author: Alpesh
+        # * Date: 09/11/2017
+        # * Reviewed By: Sunil
+        #
+        # @params [Integer] admin_id (mandatory) - logged in admin
+        # @params [Integer] page_size (optional)
+        # @params [Integer] offset (optional)
+        #
+        # @return [AdminManagement::Kyc::Dashboard::SaleDaily]
+        #
+        def initialize(params)
+          super
+
+          @admin_id = @params[:admin_id]
+          @page_size = @params[:page_size]
+          @offset = @params[:offset]
+
+          @curr_page_data = []
+          @all_time_data = {}
+        end
+
+        # perform
+        #
+        # * Author: Alpesh
+        # * Date: 09/11/2017
+        # * Reviewed By: Sunil
+        #
+        # @return [Result::Base]
+        #
+        def perform
+
+          r = validate_and_sanitize
+          return r unless r.success?
+
+          fetch_contract_events_data
+
+          set_api_response_data
+
+          success_with_data(@api_response_data)
+        end
+
+        private
+
+        # Validate and sanitize
+        #
+        # * Author: Alpesh
+        # * Date: 09/11/2017
+        # * Reviewed By: Sunil
+        #
+        # return [Result::Base]
+        #
+        def validate_and_sanitize
+          r = validate
+          return r unless r.success?
+
+          @page_size = 50 if @page_size.to_i < 1 || @page_size.to_i > 50
+          @offset = 0 if @offset.to_i <= 0
+
+          success
+        end
+
+        # Set API response data
+        #
+        # * Author: Alpesh
+        # * Date: 09/11/2017
+        # * Reviewed By: Sunil
+        #
+        # Sets @curr_page_data
+        #
+        def fetch_contract_events_data
+
+          @total_filtered_recs = ContractEvent.count
+
+          ContractEvent.order("id DESC").each do |c_e|
+
+            @curr_page_data << {
+                day_time: Time.at(c_e.created_at).strftime("%d/%m/%Y %H:%M:%S"),
+                event_name: c_e.kind,
+                contract_address: c_e.contract_address
+            }
+
+          end
+
+        end
+
+        # Set API response data
+        #
+        # * Author: Alpesh
+        # * Date: 09/11/2017
+        # * Reviewed By: Sunil
+        #
+        # Sets @api_response_data
+        #
+        def set_api_response_data
+
+          @api_response_data = {
+              meta: {
+                  page_offset: @offset,
+                  page_size: @page_size,
+                  total_filtered_recs: @total_filtered_recs
+              },
+              curr_page_data: @curr_page_data
+          }
+
+        end
+
+      end
+
+    end
+
+  end
+
+end

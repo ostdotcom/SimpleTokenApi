c5f1d12e0170e1b9256f894f0e26509c69c62277
abhay-pepo <abhay@pepo.com>
2017-11-18 19:22:20 +0530

    PC handle timeout error PC Refactor

--- lib/email/services/pepo_campaigns.rb
+++ lib/email/services/pepo_campaigns.rb
@@ -13,7 +13,6 @@
 #     <setting_2> => <value>,
 #   }
 #)
-
 module Email
 
   module Services
@@ -24,13 +23,9 @@ module Email
       require "open-uri"
       require "openssl"
 
-      attr_accessor :api_key, :api_secret, :version
-
       # Initialize
       #
-      # parameters:
-      #   api_key, String
-      #   api_secret, String
+      # Sets @api_key, @api_secret, @api_base_url, @version
       #
       def initialize
         @api_key = GlobalConstant::PepoCampaigns.api_key
@@ -44,24 +39,19 @@ module Email
       # params:
       #   name, String
       #   source, String
-      #   opt_in_type, String
+      #   opt_in_type, String, (Single Opt In / Double Opt In)
       #
       # returns:
       #   Hash, response data from server
       #
       def create_list(name, source, opt_in_type)
-        endpoint = "/api/#{version}/list/create/"
+        endpoint = "/api/#{@version}/list/create/"
         custom_params = {
-            "name" => name,
-            "source" => source,
-            "opt_in_type" => opt_in_type
+          "name" => name,
+          "source" => source,
+          "opt_in_type" => opt_in_type
         }
-
-        base_params = base_params(endpoint, custom_params)
-        uri = post_api_uri(endpoint)
-        http = setup_request(uri)
-        result = http.post(uri.path, base_params.merge(custom_params).to_query)
-        JSON.parse(result.body)
+        make_post_request(endpoint, custom_params)
       end
 
       # Add Contact to a List
@@ -76,17 +66,13 @@ module Email
       #   Hash, response data from server
       #
       def add_contact(list_id, email, attributes = {}, user_status = {})
-        endpoint = "/api/#{version}/list/#{list_id}/add-contact/"
+        endpoint = "/api/#{@version}/list/#{list_id}/add-contact/"
         custom_params = {
-            'email' => email,
-            'attributes' => attributes,
-            'user_status' => user_status
+          'email' => email,
+          'attributes' => attributes,
+          'user_status' => user_status
         }
-        base_params = base_params(endpoint, custom_params)
-        uri = post_api_uri(endpoint)
-        http = setup_request(uri)
-        result = http.post(uri.path, base_params.merge(custom_params).to_query)
-        JSON.parse(result.body)
+        make_post_request(endpoint, custom_params)
       end
 
       # Update contact
@@ -101,17 +87,13 @@ module Email
       #   Hash, response data from server
       #
       def update_contact(list_id, email, attributes = {}, user_status = {})
-        endpoint = "/api/#{version}/list/#{list_id}/update-contact/"
+        endpoint = "/api/#{@version}/list/#{list_id}/update-contact/"
         custom_params = {
-            "email" => email,
-            'attributes' => attributes,
-            'user_status' => user_status
+          "email" => email,
+          'attributes' => attributes,
+          'user_status' => user_status
         }
-        base_params = base_params(endpoint, custom_params)
-        uri = post_api_uri(endpoint)
-        http = setup_request(uri)
-        result = http.post(uri.path, base_params.merge(custom_params).to_query)
-        JSON.parse(result.body)
+        make_post_request(endpoint, custom_params)
       end
 
       # Remove contact
@@ -124,15 +106,11 @@ module Email
       #   Hash, response data from server
       #
       def remove_contact(list_id, email)
-        endpoint = "/api/#{version}/list/#{list_id}/remove-contact/"
+        endpoint = "/api/#{@version}/list/#{list_id}/remove-contact/"
         custom_params = {
-            "email" => email
+          "email" => email
         }
-        base_params = base_params(endpoint, custom_params)
-        uri = post_api_uri(endpoint)
-        http = setup_request(uri)
-        result = http.post(uri.path, base_params.merge(custom_params).to_query)
-        JSON.parse(result.body)
+        make_post_request(endpoint, custom_params)
       end
 
       # Change user status
@@ -145,27 +123,28 @@ module Email
       #   Hash, response data from server
       #
       def change_user_status(email, type)
-        endpoint = "/api/#{version}/user/change-status/"
+        endpoint = "/api/#{@version}/user/change-status/"
         custom_params = {
-            'email' => email,
-            'type' => type
+          'email' => email,
+          'type' => type
         }
-        base_params = base_params(endpoint,custom_params)
-        uri = post_api_uri(endpoint)
-        http = setup_request(uri)
-        result = http.post(uri.path, base_params.merge(custom_params).to_query)
-        JSON.parse(result.body)
+        make_post_request(endpoint, custom_params)
       end
 
+      # Fetch User Info
+      #
+      # params:
+      #   emails, Array
+      #
+      # returns:
+      #   Hash, response data from server
+      #
       def fetch_users_info(emails)
-        endpoint = "/api/#{version}/user/info/"
+        endpoint = "/api/#{@version}/user/info/"
         custom_params = {
-            'emails' => emails.join(',')
+          'emails' => emails.join(',')
         }
-        base_params = base_params(endpoint, custom_params)
-        raw_url = get_api_url(endpoint) + "?#{base_params.merge(custom_params).to_query}"
-        result = URI.parse(raw_url).read
-        JSON.parse(result)
+        make_get_request(endpoint, custom_params)
       end
 
       # Send transactional email
@@ -179,57 +158,73 @@ module Email
       #   Hash, response data from server
       #
       def send_transactional_email(email, template, email_vars)
-        endpoint = "/api/#{version}/send/"
+        endpoint = "/api/#{@version}/send/"
         custom_params = {
-            "email" => email,
-            "template" => template,
-            "email_vars" => email_vars.to_json
+          "email" => email,
+          "template" => template,
+          "email_vars" => email_vars.to_json
         }
-
-        base_params = base_params(endpoint, custom_params)
-        uri = post_api_uri(endpoint)
-        http = setup_request(uri)
-        result = http.post(uri.path, base_params.merge(custom_params).to_query)
-        JSON.parse(result.body)
+        make_post_request(endpoint, custom_params)
       end
 
+      # Get Transactional Send Info
+      #
+      # params:
+      #   send_id, String
+      #
+      # returns:
+      #   Hash, response data from server
+      #
       def get_send_info(send_id)
-        endpoint = "/api/#{version}/get-send/"
-        custom_params = {"send_id" => send_id}
-        base_params = base_params(endpoint, custom_params)
-        raw_url = get_api_url(endpoint) + "?#{base_params.merge(custom_params).to_query}"
-        result = URI.parse(raw_url).read
-        JSON.parse(result)
-      end
-
-      def fetch_custom_attributes
-        endpoint = "/api/#{version}/custom-attributes/"
-        base_params = base_params(endpoint)
-        raw_url = get_api_url(endpoint) +"?#{base_params.to_query}"
-        result = URI.parse(raw_url).read
-        JSON.parse(result)
-      end
-
-      def create_custom_attribute(name, type, options={})
-        endpoint = "/api/#{version}/custom-attribute/create/"
+        endpoint = "/api/#{@version}/get-send/"
         custom_params = {
-            "name" => name,
-            "type" => type,
-            "fallback" => options[:fallback]
+          "send_id" => send_id
         }
-        base_params = base_params(endpoint, custom_params)
-        uri = post_api_uri(endpoint)
-        http = setup_request(uri)
-        result = http.post(uri.path, base_params.merge(custom_params).to_query)
-        JSON.parse(result.body)
+        make_get_request(endpoint, custom_params)
+      end
+
+      # List All Custom Attributes of an Account
+      #
+      # returns:
+      #   Hash, response data from server
+      #
+      def fetch_custom_attributes
+        endpoint = "/api/#{@version}/custom-attributes/"
+        make_get_request(endpoint)
+      end
+
+      # Create New Custom Attribute
+      #
+      # params:
+      #   name, String
+      #   type, String
+      #   options, Hash
+      #
+      # returns:
+      #   Hash, response data from server
+      #
+      def create_custom_attribute(name, type, options={})
+        endpoint = "/api/#{@version}/custom-attribute/create/"
+        custom_params = {
+          "name" => name,
+          "type" => type,
+          "fallback" => options[:fallback]
+        }
+        make_post_request(endpoint, custom_params)
       end
 
       private
 
+      # Create Request Data
+      #
+      # params:
+      #   uri, URI object
+      #
+      # returns:
+      #   http, Net::HTTP object
+      #
       def setup_request(uri)
         http = Net::HTTP.new(uri.host, uri.port)
-        http.read_timeout = 5
-        http.open_timeout = 5
         if uri.scheme == "https"
           http.use_ssl = true
           http.verify_mode = OpenSSL::SSL::VERIFY_PEER
@@ -237,26 +232,113 @@ module Email
         http
       end
 
+      # Create Base Params
+      #
+      # params:
+      #   endpoint, String
+      #   custom_params, Hash
+      #
+      # returns:
+      #   Hash, Request Data
+      #
       def base_params(endpoint, custom_params={})
         request_time = DateTime.now.to_s
         query_param = custom_params.merge("request-time" => request_time).to_query.gsub(/^&/, '')
         str = "#{endpoint}?#{query_param}"
-        signature = generate_signature(api_secret, str)
-        {"request-time" => request_time, "signature" => signature, "api-key" => api_key}
+        {
+          "request-time" => request_time,
+          "signature" => generate_signature(str),
+          "api-key" => @api_key
+        }
       end
 
-      def generate_signature(api_secret, string_to_sign)
+      # Generate Signature
+      #
+      # params:
+      #   string_to_sign, String
+      #
+      # returns:
+      #   String, HexDigest
+      #
+      def generate_signature(string_to_sign)
         digest = OpenSSL::Digest.new('sha256')
-        puts "--------string_to_sign=>#{string_to_sign}-----"
-        OpenSSL::HMAC.hexdigest(digest, api_secret, string_to_sign)
+        Rails.logger.info("--------string_to_sign=>#{string_to_sign}-----")
+        OpenSSL::HMAC.hexdigest(digest, @api_secret, string_to_sign)
       end
 
+      # Post API URI object
+      #
+      # params:
+      #   endpoint, String
+      #
+      # returns:
+      #   Object, URI object
+      #
       def post_api_uri(endpoint)
         URI(@api_base_url + endpoint)
       end
 
+      # Get API Url
+      #
+      # params:
+      #   endpoint, String
+      #
+      # returns:
+      #   String
+      #
       def get_api_url(endpoint)
         @api_base_url + endpoint
+      end
+
+      # Make Get Request
+      #
+      # params:
+      #   endpoint, String
+      #   custom_params, Hash
+      #
+      # returns:
+      #   Hash, Response
+      #
+      def make_get_request(endpoint, custom_params = {})
+        base_params = base_params(endpoint, custom_params)
+        raw_url = get_api_url(endpoint) + "?#{base_params.merge(custom_params).to_query}"
+
+        begin
+          Timeout.timeout(GlobalConstant::PepoCampaigns.api_timeout) do
+            result = URI.parse(raw_url).read
+            return JSON.parse(result)
+          end
+        rescue Timeout::Error => e
+          return {"error" => "Timeout Error", "message" => "Error: #{e.message}"}
+        rescue => e
+          return {"error" => "Exception: Something Went Wrong", "message" => "Exception: #{e.message}"}
+        end
+      end
+
+      # Make Post Request
+      #
+      # params:
+      #   endpoint, String
+      #   custom_params, Hash
+      #
+      # returns:
+      #   Hash, Response
+      #
+      def make_post_request(endpoint, custom_params = {})
+        base_params = base_params(endpoint, custom_params)
+        uri = post_api_uri(endpoint)
+        begin
+          Timeout.timeout(GlobalConstant::PepoCampaigns.api_timeout) do
+            http = setup_request(uri)
+            result = http.post(uri.path, base_params.merge(custom_params).to_query)
+            return JSON.parse(result.body)
+          end
+        rescue Timeout::Error => e
+          return {"error" => "Timeout Error", "message" => "Error: #{e.message}"}
+        rescue => e
+          return {"error" => "Something Went Wrong", "message" => "Exception: #{e.message}"}
+        end
+
       end
 
     end

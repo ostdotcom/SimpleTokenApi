b2ef184ed977bf8b15a0cd3608802afa25487024
aman_barbaria <aman@pepo.com>
2018-01-24 19:32:16 +0530

    remove autoresponder for partner temporary

--- app/services/user_management/contact_us/partner.rb
+++ app/services/user_management/contact_us/partner.rb
@@ -44,6 +44,10 @@ module UserManagement
         "#{GlobalConstant::Email.subject_prefix}Partner Companies - Contact Us - #{@email}"
       end
 
+      def send_autorespond_email
+
+      end
+
       # autorespond template name
       #
       # * Author: Aman

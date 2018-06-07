32a19d902b9ed1196e0e3fa1cd97beeefdfef5ad
pankajkeswani <pankaj@pepo.com>
2018-06-07 15:14:36 +0530

    Delted old contact us

--- app/controllers/web/static/home_controller.rb
+++ app/controllers/web/static/home_controller.rb
@@ -5,27 +5,6 @@ class Web::Static::HomeController < Web::Static::BaseController
   prepend_before_action :merge_utm_to_params, only: [:contact_us_pipe_drive_kyc]
 
   #
-  # * Author: Aman
-  # * Date: 18/01/2018
-  # * Reviewed By:
-  #
-  def contact_us_partners
-    service_response = UserManagement::ContactUs::Partner.new(params).perform
-    render_api_response(service_response)
-  end
-
-
-  #
-  # * Author: Aman
-  # * Date: 24/01/2018
-  # * Reviewed By:
-  #
-  def contact_us_kyc
-    service_response = UserManagement::ContactUs::Kyc.new(params).perform
-    render_api_response(service_response)
-  end
-
-  #
   # * Author: Santhosh
   # * Date: 21/05/2018
   # * Reviewed By:

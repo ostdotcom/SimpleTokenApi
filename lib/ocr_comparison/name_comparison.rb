f1b96fb12947f583d453ac515f2f81e37dd55043
Tejas-Sangani <tejas@ost.com>
2018-06-29 19:11:14 +0530

    first_name and last_name match code for password added.

--- lib/ocr_comparison/name_comparison.rb
+++ lib/ocr_comparison/name_comparison.rb
@@ -18,22 +18,56 @@ module OcrComparison
       end
 
       if percent_match < 100
-        matched_words, total_count = [], 0
 
-        @match_string.split(" ").each do |word|
-          total_count += 1
-          @safe_line_array.split("\n").each do |line|
-            next if line.blank?
-            if line.match(/\b#{word}\b/i)
-              matched_words << word
-            end
-          end
-        end
+        matched_words, total_count = check_name_by_split
 
+        puts "matched_words : #{matched_words}"
         percent_match = 100 if total_count == matched_words.count
       end
 
       percent_match
+    end
+
+
+    def check_name_by_split
+
+      matched_words, total_count = [], 0
+
+      @match_string.split(" ").each do |word|
+        total_count += 1
+        word = word.downcase
+        @safe_line_array.split("\n").each do |line|
+          next if line.blank?
+
+          if line.match(/\b#{word}\b/i)
+            puts "word #{word} matches in line : #{line}"
+            matched_words << word
+            break
+          else
+            passport_words_array = passport_string(line)
+            passport_words_array.each do |passport_word|
+              puts "inside passport_string : #{line}"
+
+              if passport_word.match(/\b#{word}\b/i)
+                puts "got word from passport : #{passport_word}"
+                matched_words << word
+                break
+              end
+            end
+          end
+        end
+      end
+
+      puts "matched_words : #{matched_words} and total_count : #{total_count}"
+      return matched_words, total_count
+
+    end
+
+
+    def passport_string(line)
+
+      line.match(/([A-Za-z]+<+)+/).to_s.split("<")
+
     end
 
   end

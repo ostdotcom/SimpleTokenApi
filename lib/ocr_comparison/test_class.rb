101eaf31ee546f3bf6be62511dec72de536fb465
Aniket <aniket@pepo.com>
2018-07-02 11:45:01 +0530

    test data.

--- lib/ocr_comparison/test_class.rb
+++ lib/ocr_comparison/test_class.rb
@@ -1,10 +1,15 @@
-module OcrMatch
-  # OcrMatch::TestClass.test
+module OcrComparison
+  # OcrComparison::TestClass.test
   class TestClass
     def self.test
 
       count = 0
-      VisionCompareText.where("first_name_match_percent<100").limit(10).each do |obj|
+      failure_array = []
+      # "document_id_number_match_percent<100"
+      # # "first_name_match_percent<100"
+      # case_id:7300
+      #
+      VisionCompareText.all.each do |obj|
         puts obj.case_id
 
         next if obj.debug_data.blank? || obj.debug_data[:err].present? || obj.debug_data[:error_type].present?
@@ -12,19 +17,21 @@ module OcrMatch
         not_match_data = obj.debug_data[:not_match_data]
         word_array  = obj.debug_data[:words_array]
 
-        first_name = not_match_data[:first_name]
+        first_name = not_match_data[:birthdate]
 
         puts "first_name :  #{first_name}"
 
 
-        first_name_match_percent = OcrMatch::NameMatch.new(word_array,first_name).perform
+        first_name_match_percent = OcrComparison::BirthdateComparison.new(word_array,first_name).perform
 
-
-        count +=1 if first_name_match_percent == 100
         puts "first_name_match_percent : #{first_name_match_percent}"
+
+        count +=1 if first_name_match_percent > 0
+
       end
 
       puts "modified count : #{count}"
+
 
     end
   end

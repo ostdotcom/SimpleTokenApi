28be234c701d0688e2d815f95a148cc331ef712e
aman-barbaria <amanbarbaria@gmail.com>
2018-07-17 21:45:36 +0530

    migration change

--- db/migrate/20180717123050_modify_auto_approve_failed_reasons_column.rb
+++ db/migrate/20180717123050_modify_auto_approve_failed_reasons_column.rb
@@ -1,13 +1,13 @@
 class ModifyAutoApproveFailedReasonsColumn < DbMigrationConnection
   def up
     run_migration_for_db(EstablishSimpleTokenUserDbConnection.config_key) do
-      change_column :user_kyc_comparison_details, :kyc_auto_approved_status, :integer , null: false, default: 0
+      change_column :user_kyc_comparison_details, :auto_approve_failed_reasons, :integer , null: false, default: 0
     end
   end
 
   def down
     run_migration_for_db(EstablishSimpleTokenUserDbConnection.config_key) do
-      change_column :user_kyc_comparison_details, :kyc_auto_approved_status, :integer, null: true
+      change_column :user_kyc_comparison_details, :auto_approve_failed_reasons, :integer, null: true
     end
   end
 end

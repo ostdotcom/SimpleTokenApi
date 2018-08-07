a6e246a04ffc114cc0d973616e027b843f0524c3
Aniket <aniket@pepo.com>
2018-08-07 15:08:57 +0530

    html validator modified.

--- lib/util/html_sanitizer.rb
+++ lib/util/html_sanitizer.rb
@@ -16,118 +16,85 @@ module Util
     # @return [Util::HtmlSanitizer]
     #
     def initialize(params)
-      @elements = params[:elements] ||= ['html', 'p', 'body', 'a','i','br','b']
-      @attributes = params[:attributes] ||= {'a': ['href','target']}
+      @elements = params[:elements] ||= ['html', 'p', 'body', 'a', 'i', 'br', 'b', 'u']
+      @attributes = params[:attributes] ||= {'a': ['href', 'target']}
       @html = params[:html]
-      @val = 0
     end
 
     def perform
       nokogiri_html = Nokogiri::HTML(@html)
-
-      is_valid_html = valid_nokogiri?(nokogiri_html)
-
+      is_valid_html = trigger_function(nokogiri_html)
       puts "is_valid_html : #{is_valid_html}"
-    end
-
-    private
-
-    def valid_nokogiri?(nokogiri_obj)
-      puts "\nnokogiriobj: #{nokogiri_obj}"
-
-      trigger_function(nokogiri_obj)
+      is_valid_html
+    rescue
+      puts "error occored while converting html"
+      false
     end
 
 
-    def trigger_function(nokogiri_obj)
-      puts "val = #{@val}"
-      @val +=1
+    def trigger_function(obj)
+      puts "\ntrigger function for : #{obj}"
 
-      puts "trigger function for : #{nokogiri_obj}"
-      if nokogiri_obj.class == Nokogiri::HTML::Document
-        puts "\n NOKOGIRI_CLASS_DOCUMENT"
-        return trigger_function(nokogiri_obj.children)
+      if obj.class == Nokogiri::HTML::Document
+        puts "NOKOGIRI_CLASS_DOCUMENT"
+        return trigger_function(obj.children)
 
-      elsif nokogiri_obj.class == Nokogiri::XML::DTD
-        puts "\n NOKOGIRI_CLASS_DTD"
+      elsif obj.class == Nokogiri::XML::DTD
+        puts "NOKOGIRI_CLASS_DTD \nreturning: true"
         return true
 
-      elsif nokogiri_obj.class == Nokogiri::XML::Element
-        puts "\n NOKOGIRI_CLASS_ELEMENT"
-        return valid_nokogiri_element?(nokogiri_obj)
+      elsif obj.class == Nokogiri::XML::Element
+        puts "NOKOGIRI_CLASS_ELEMENT"
+        return valid_element?(obj)
 
-      elsif nokogiri_obj.class == Nokogiri::XML::Text
-        puts "\n NOKOGIRI_CLASS_TEXT"
+      elsif obj.class == Nokogiri::XML::Text
+        puts "NOKOGIRI_CLASS_TEXT \nreturning: true"
         return true
 
-      elsif nokogiri_obj.class == Nokogiri::XML::Attr
-        puts "\n NOKOGIRI_CLASS_ATTR"
-        return valid_nokogiri_attr?(nokogiri_obj.name, nokogiri_obj.attributes)
+      elsif obj.class == Nokogiri::XML::Attr
+        puts "NOKOGIRI_CLASS_ATTR"
+        return valid_attr?(obj.name, obj.attributes)
 
-      elsif nokogiri_obj.class == Nokogiri::XML::NodeSet
-        puts "\n NOKOGIRI_CLASS_NODESET"
-        return valid_nokogiri_nodeset?(nokogiri_obj.children)
+      elsif obj.class == Nokogiri::XML::NodeSet
+        puts "NOKOGIRI_CLASS_NODESET"
+        return valid_nodeset?(obj)
       else
         puts "\nnokogiri class not found"
       end
-
-      # case nokogiri_obj.class
-      #   when Nokogiri::HTML::Document
-      #     puts "\n NOKOGIRI_CLASS_DOCUMENT"
-      #     return valid_nokogiri_nodeset?(nokogiri_obj.children)
-      #
-      #   when Nokogiri::XML::DTD
-      #     puts "\n NOKOGIRI_CLASS_DTD"
-      #     return true
-      #
-      #   when Nokogiri::XML::Element
-      #     puts "\n NOKOGIRI_CLASS_ELEMENT"
-      #     return valid_nokogiri_element?(nokogiri_obj)
-      #
-      #   when Nokogiri::XML::Text
-      #     puts "\n NOKOGIRI_CLASS_TEXT"
-      #     return true
-      #
-      #   when Nokogiri::XML::Attr
-      #     puts "\n NOKOGIRI_CLASS_ATTR"
-      #     return valid_nokogiri_attr?(nokogiri_obj.name, nokogiri_obj.attributes)
-      #
-      #   when Nokogiri::XML::NodeSet
-      #     puts "\n NOKOGIRI_CLASS_NODESET"
-      #     return valid_nokogiri_nodeset?(nokogiri_obj.children)
-      #
-      #   else
-      #     puts "\nnokogiri class not found"
-      # end
     end
 
-    def valid_nokogiri_nodeset?(node_set)
+    def valid_nodeset?(node_set)
       puts "node_set : #{node_set}"
       node_set.each_with_index do |obj, index|
-        puts "node_set_obj : #{obj} and index : #{index}"
+        puts "index : #{index} and obj : #{obj}"
         is_success = trigger_function(obj)
-        puts "is_success : #{is_success}"
-        return false unless is_success
+        if !is_success
+          puts "returning false for #{obj}"
+          return false
+        end
       end
       true
     end
 
-    def valid_nokogiri_element?(element)
-      puts "element : #{element}"
+    def valid_element?(element)
+      puts "element : #{element} and class: #{element.name}"
       if @elements.include?(element.name)
-        return false if !valid_nokogiri_attr?(element.name, element.attributes)
+        if !valid_attributes?(element.name, element.attributes)
+          puts "returning false because attributes didn't match"
+          return false
+        end
         puts "triggering : #{element.children}"
         return trigger_function(element.children)
       end
-
+      puts "element //#{element.name}// not allowed"
       false
     end
 
-    def valid_nokogiri_attr?(ele_name, attributes)
+    def valid_attributes?(ele_name, attributes)
       ele_attributes = @attributes[ele_name.to_sym] || []
-      puts "ele_name : #{ele_name} and attributes : #{attributes} and ele_attributes : #{ele_attributes}"
+      puts "ele_name : #{ele_name}\nelement_attributes : #{attributes.keys}\nattributes_required: #{ele_attributes}"
 
-      ele_attributes == attributes.keys
+      (ele_attributes - attributes.keys).length == 0
     end
 
   end

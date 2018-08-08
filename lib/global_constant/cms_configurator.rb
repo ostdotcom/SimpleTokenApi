aaa8dcfb70af8b2cc82394ac57edebe03d32a03a
Aniket <aniket@pepo.com>
2018-08-08 19:39:47 +0530

    cms configurator global constant added.

---
+++ lib/global_constant/cms_configurator.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+module GlobalConstant
+  class CmsConfigurator
+
+    class << self
+
+
+      ######### YML keys and values #############
+
+      def data_kind_key
+        'data_kind'
+      end
+
+      def required_key
+        'required'
+      end
+
+      def validations_key
+        'validations'
+      end
+
+      def value_color
+        'color'
+      end
+
+      def value_text
+        'text'
+      end
+
+      def value_file
+        'file'
+      end
+
+      def value_number
+        'number'
+      end
+
+      def value_array
+        'array'
+      end
+
+      ######### YML keys and values end#############
+
+
+      def get_dashboard_yml
+        @dashboard_yml ||= YAML.load_file(open(Rails.root.to_s + '/config/fe_configurator/dashboard.yml'))
+      end
+
+      def get_kyc_form_yml
+        @kyc_form_yml ||= YAML.load_file(open(Rails.root.to_s + '/config/fe_configurator/kyc_form.yml'))
+
+      end
+
+      def get_registration_yml
+        @registration_yml ||= YAML.load_file(open(Rails.root.to_s + '/config/fe_configurator/registration.yml'))
+      end
+
+      def get_theme_yml
+        @theme_yml ||= YAML.load_file(open(Rails.root.to_s + '/config/fe_configurator/theme.yml'))
+      end
+
+    end
+
+  end
+end

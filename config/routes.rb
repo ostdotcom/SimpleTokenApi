3d4bbad03e7b490e6ec9097de06f82c2d5beeb08
Tejas-Sangani <tejas@ost.com>
2018-08-23 20:10:54 +0530

    Created service for Alpha4 signup.

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,7 @@ Rails.application.routes.draw do
     end
 
     scope 'api/home', controller: 'web/static/home' do
-      match '/alpha3/signup' => :register_for_alpha3, via: [:GET, :POST]
-      match '/alpha3/submission' => :submission_for_alpha3, via: [:GET, :POST]
+      match '/alpha4/signup' => :register_for_alpha4, via: [:GET, :POST]
     end
 
     match '*permalink', to: 'application#not_found', via: :all

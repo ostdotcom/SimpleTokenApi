f7595818cc3ec7ba15f06a0bd803bb957a2bc828
Pankaj Keswani <pankaj@Pankajs-MacBook-Pro-2.local>
2018-09-24 20:51:39 +0530

    Few changes with result base

--- app/controllers/rest_api/saas_api/v2/base_controller.rb
+++ app/controllers/rest_api/saas_api/v2/base_controller.rb
@@ -12,4 +12,15 @@ class RestApi::SaasApi::V2::BaseController < RestApi::SaasApi::BaseController
     Authentication::ApiRequest::V2
   end
 
+  # Sanitize and reformat Error response as per old response
+  # NOT APPLICABLE for new services V2 Onwards
+  #
+  # * Author: Pankaj
+  # * Date: 18/09/2018
+  # * Reviewed By:
+  #
+  def reformat_as_old_response(response_hash)
+    response_hash
+  end
+
 end

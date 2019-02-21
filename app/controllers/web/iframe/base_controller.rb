eedc0ab6130ff516abd5e2bca45542419806165b
aman-barbaria <amanbarbaria@gmail.com>
2019-02-21 22:41:43 +0530

    iframe routes added

--- app/controllers/web/iframe/base_controller.rb
+++ app/controllers/web/iframe/base_controller.rb
@@ -1,20 +1,25 @@
 class Web::Iframe::BaseController < Web::WebController
 
-  before_action :authenticate_request
   before_action :authenticate_client_host
-
+  before_action :authenticate_request
 
   private
 
-  # Validate cookie
+  # Validate referer host to be one of our clients
   #
   # * Author: Aman
   # * Date: 01/02/2018
   # * Reviewed By:
   #
   def authenticate_client_host
-    # check parent url of client
-    # params[:client_id] = 1
+    service_response = Authentication::Client::VerifyIframeReferer.new(domain: request.host).perform
+
+    if service_response.success?
+      params[:client_id] = service_response.data[:client_id]
+      service_response.data = {}
+    else
+      render_api_response(service_response)
+    end
   end
 
   # Validate cookie
@@ -24,8 +29,9 @@ class Web::Iframe::BaseController < Web::WebController
   # * Reviewed By: Sunil
   #
   def authenticate_request
-    # check token and get user id
-    # params[:user_id] = 1
+    params[:user_id] = params[:token].present? ? params[:token].to_i :
+                           UserKycDetail.active_kyc.where(client_id: params[:client_id]).
+                               where.not.(aml_status: GlobalConstant::UserKycDetail.aml_approved_statuses).last.user_id
   end
 
 end
---
+++ app/services/authentication/client/verify_iframe_referer.rb
@@ -0,0 +1,72 @@
+module Authentication
+  module Client
+
+    class VerifyIframeReferer
+
+      # Initialize
+      #
+      # * Author: Aman
+      # * Date: 01/02/2018
+      # * Reviewed By:
+      #
+      # @param [String] referer_host (mandatory) - this is the domain in which iframe was loaded
+      #
+      # @return [Authentication::Client::VerifyIframeReferer]
+      #
+      def initialize(params)
+        super
+
+        @referer_host = @params[:referer_host]
+        @client_id = nil
+      end
+
+      # Perform
+      #
+      # * Author: Aman
+      # * Date: 01/02/2018
+      # * Reviewed By:
+      #
+      # @return [Result::Base]
+      #
+      def perform
+        r = validate
+        return r unless r.success?
+
+        r = fetch_client_details
+        return r unless r.success?
+
+        success_with_data(
+            client_id: @client_id
+        )
+
+      end
+
+      private
+
+      # Set client id from domain
+      #
+      # * Author: Aman
+      # * Date: 01/02/2018
+      # * Reviewed By:
+      #
+      # Sets @client_id
+      #
+      # @return [Result::Base]
+      #
+      def fetch_client_details
+        client_web_host_detail_obj = ClientWebHostDetail.get_from_memcache_by_domain(@domain)
+
+
+        if client_web_host_detail_obj.blank? || (client_web_host_detail_obj.status !=
+            GlobalConstant::ClientWebHostDetail.active_status)
+          return error_with_identifier('invalid_or_expired_token', 'a_c_vir_fcd_1')
+        end
+
+        @client_id = client_web_host_detail_obj.client_id
+
+        success
+      end
+    end
+
+  end
+end
